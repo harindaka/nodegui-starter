9994f41dfedc95bd62e77dca5c29edff308b9865
Hemal Athulathmudali <harindaka@gmail.com>
2020-04-18 23:43:11 +0530

    added stylesheet

---
+++ src/styles.ts
@@ -0,0 +1,21 @@
+const styles = `
+
+  QPushButton {
+    padding: 10px;
+  }
+
+  #myroot {
+    background-color: #009688;
+    height: '100%';
+    align-items: 'center';
+    justify-content: 'center';
+  }
+  #mylabel {
+    font-size: 16px;
+    font-weight: bold;
+    padding: 1;
+  }
+
+`
+
+export { styles };
